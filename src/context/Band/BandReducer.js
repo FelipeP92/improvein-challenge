24b095d7ce5485b3c08bc560711f195a608d55ad
FelipeP92 <feelipee.p36@gmail.com>
2021-06-02 00:43:10 -0300

    .

--- src/context/Band/BandReducer.js
+++ src/context/Band/BandReducer.js
@@ -1,4 +1,4 @@
-import { GET_BANDS, GET_ALBUMS} from '../type';
+import { GET_BANDS, GET_ALBUMS} from '../types';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
